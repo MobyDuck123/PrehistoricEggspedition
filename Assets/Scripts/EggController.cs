7b3e6905632e9088052e961d42ed49d9f7f57a73
MobyDuck123 <152602889+MobyDuck123@users.noreply.github.com>
2023-12-22 21:23:41 +0800

    Whoops
    this is the actual submission

--- Assets/Scripts/EggController.cs
+++ Assets/Scripts/EggController.cs
@@ -1,29 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class EggController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Biomes eggBiome;
+    public GameObject[] dinosaurPrefabs; // Array of dinosaur prefabs for each biome
+    public GameObject dinosaurBiome;
+
+    private void GetDinosaurBiome()
     {
-
+        for (int i = 0; i < dinosaurPrefabs.Length; i++)
+        {
+            Debug.Log("This dinosaur is" + dinosaurPrefabs[i].name);
+            Biomes tempDinoBiome = dinosaurPrefabs[i].GetComponent<DinosaurBiome>().GetDinoBiome();
+            Debug.Log("Temporary Dino biome is" + tempDinoBiome);
+           
+            if (tempDinoBiome == eggBiome)
+            {
+                dinosaurBiome = dinosaurPrefabs[i];
+                Debug.Log("DinosaurBiome is" + dinosaurPrefabs[i]);
+            }
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // If the colliding object is tagged as "player", make this object disappear
-            gameObject.SetActive(false);
+
+            if (dinosaurBiome != null)
+            {
+                Instantiate(dinosaurBiome, transform.position, Quaternion.identity);
+
+                Debug.Log("Egg is in biome: " + eggBiome);
+
+                // Destroy the egg
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.LogError("Dinosaur biome not set");
+            }
         }
     }
 
+    public void SetBiome(Biomes biome)
+    {
+        eggBiome = biome;
+        Debug.Log("My biome is " + eggBiome);
+        GetDinosaurBiome();
+    }
 }
 
+//// private void OnTriggerEnter2D(Collider2D collision)
+//{
+//    if (collision.gameObject.CompareTag("Player"))
+//    {
+//        // Check the biome of the egg using the information set in SetBiome
+//        int biomeIndex = (int)eggBiome;
+
+//        if (biomeIndex >= 0 && biomeIndex < dinosaurPrefabs.Length)
+//        {
+//            // Log the biome information for debugging
+//            Debug.Log("Egg is in biome: " + eggBiome);
+
+//            // Instantiate the corresponding dinosaur prefab at the egg's position
+//            Instantiate(dinosaurPrefabs[biomeIndex], transform.position, Quaternion.identity);
+
+//            // Destroy the egg
+//            Destroy(gameObject);
+//        }
+//        else
+//        {
+//            Debug.LogError("Invalid biome index");
+//        }
+//    }
+//}
--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,8 @@ using UnityEngine.Tilemaps;
 
 public class SpawnController : MonoBehaviour
 {
-    public GameObject eggPrefab; // Reference to the egg prefab
-    public Tilemap tilemap; // Reference to your Tilemap containing the biomes
+    public GameObject eggPrefab; // Reference to the egg prefab - make sure they appear in inspector 
+    public Tilemap tilemap; // Reference to your Tilemap containing the biomes - make sure they appear in inspector 
 
     void Start()
     {
@@ -15,34 +16,105 @@ public class SpawnController : MonoBehaviour
 
     void SpawnEggsOnBiomes()
     {
-        BoundsInt bounds = tilemap.cellBounds;
+        BoundsInt bounds = tilemap.cellBounds; // Defining the boundary as cells on the tile map
 
-        // Create a list to keep track of the tile types already processed
-        // This ensures only one egg is spawned per unique tile type
-        List<Sprite> processedTiles = new List<Sprite>();
+        // Create a dictionary to keep track of the tiles for each biome
+        Dictionary<Sprite, List<Vector3Int>> biomeTiles = new Dictionary<Sprite, List<Vector3Int>>();
 
         // Loop through each cell in the Tilemap
-        foreach (Vector3Int cellPosition in bounds.allPositionsWithin)
+        foreach (Vector3Int cellPosition in bounds.allPositionsWithin) //allPositionWithin is just the entire boundary
         {
             TileBase tile = tilemap.GetTile(cellPosition);
 
             // Check if the tile is not null (i.e., there is a tile at this position)
-            if (tile != null && tile is Tile)  // Ensure the tile is of type Tile
+            if (tile != null && tile is Tile)  // Make sure that the tile is in fact a Tile, if this isnt here weird shit occurs 
             {
                 Sprite tileSprite = ((Tile)tile).sprite;
 
-                // Check if this tile type has already been processed
-                if (!processedTiles.Contains(tileSprite))
+                // If the biome is not in the dictionary, add it
+                if (!biomeTiles.ContainsKey(tileSprite))
                 {
-                    // Spawn an egg at the center of the tile
-                    Vector3 tileCenter = tilemap.GetCellCenterWorld(cellPosition);
-                    Instantiate(eggPrefab, tileCenter, Quaternion.identity);
-
-                    // Mark this tile type as processed
-                    processedTiles.Add(tileSprite);
+                    biomeTiles[tileSprite] = new List<Vector3Int>();
                 }
+
+                // Add the cell position to the list for this biome
+                biomeTiles[tileSprite].Add(cellPosition);
+            }
+        }
+
+        // Loop through each biome and spawn one egg randomly
+        foreach (var kvp in biomeTiles) // Iterate through each key-value pair in the biomeTiles dictionary
+        {
+            List<Vector3Int> tilesInBiome = kvp.Value; // Retrieve the list of Vector3Int tiles associated with the current biome
+            Biomes tempBiome = Biomes.None; // Initialize a temporary variable to store the current biome (default to Biomes.None)
+
+            foreach (Biomes biome in Enum.GetValues(typeof(Biomes))) // Cycle through each value in the Biomes enum
+            {
+                if (kvp.Key.ToString().Contains(biome.ToString())) // Check if the returns the same biome name. Biome data here is saved as a SPRITE
+
+                {
+                    tempBiome = biome; // If true, set tempBiome to the current biome
+                }
+            }
+
+            // Ensure there are tiles in the biome before spawning an egg
+            if (tilesInBiome.Count > 0)
+            {
+                // Randomly select a tile from the biome
+                Vector3Int randomTilePosition = tilesInBiome[UnityEngine.Random.Range(0, tilesInBiome.Count)];
+
+                // Spawn an egg at the center of the randomly selected tile
+                Vector3 tileCenter = tilemap.GetCellCenterWorld(randomTilePosition);
+
+
+                GameObject egg = Instantiate(eggPrefab, tileCenter, Quaternion.identity);
+
+                egg.GetComponent<EggController>().SetBiome(tempBiome);
+            }
+        }
+
+        foreach (var kvp in biomeTiles)
+        {
+            List<Vector3Int> tilesInBiome = kvp.Value;
+            Biomes tempBiome = Biomes.None;
+
+            string spriteName = kvp.Key.name;  
+
+            Debug.Log("Sprite Name: " + spriteName);  
+
+            if (spriteName == "DeepWater")
+                tempBiome = Biomes.DeepWater;
+            else if (spriteName == "Plains")
+                tempBiome = Biomes.Plains;
+            else if (spriteName == "FreshWater")
+                tempBiome = Biomes.FreshWater;
+            else if (spriteName == "Dirt")
+                tempBiome = Biomes.Dirt;
+            else if (spriteName == "Forest")
+                tempBiome = Biomes.Forest;
+            else if (spriteName == "Snow")
+                tempBiome = Biomes.Snow;
+            else if (spriteName == "Sand")
+                tempBiome = Biomes.Sand;
+
+            Debug.Log("Temp Biome: " + tempBiome);  
+
+            // Ensure there are tiles in the biome before spawning an egg
+            if (tilesInBiome.Count > 0)
+            {
+                // Randomly select a tile from the biome
+                Vector3Int randomTilePosition = tilesInBiome[UnityEngine.Random.Range(0, tilesInBiome.Count)];
+
+                // Spawn an egg at the center of the randomly selected tile
+                Vector3 tileCenter = tilemap.GetCellCenterWorld(randomTilePosition);
+
+                GameObject egg = Instantiate(eggPrefab, tileCenter, Quaternion.identity);
+
+                egg.GetComponent<EggController>().SetBiome(tempBiome);
             }
         }
     }
 
 }
+
+
